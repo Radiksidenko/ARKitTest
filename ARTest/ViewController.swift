8f85b25277cfb09474207a7cd31c3e973cf36854
Radiksidenko <radik-sidehnko@ukr.net>
2018-05-20 22:39:33 +0300

    add pac-man

--- ARTest/ViewController.swift
+++ ARTest/ViewController.swift
@@ -33,75 +33,103 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     
     func addBox() {
-        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
         
-        let boxNode1 = SCNNode()
-        let boxNode = SCNNode()
-        let boxNode2 = SCNNode()
+        let line = SCNBox(width: 0.05, height: 0.01, length: 0.01, chamferRadius: 0)
+        let line2 = SCNBox(width: 0.09, height: 0.01, length: 0.01, chamferRadius: 0)
+        let line3 = SCNBox(width: 0.11, height: 0.02, length: 0.01, chamferRadius: 0)
+        let line4 = SCNBox(width: 0.1, height: 0.01, length: 0.01, chamferRadius: 0)
+        let line5 = SCNBox(width: 0.07, height: 0.01, length: 0.01, chamferRadius: 0)
+        let line6 = SCNBox(width: 0.04, height: 0.01, length: 0.01, chamferRadius: 0)
         
-        boxNode.geometry = box
-        boxNode.position = SCNVector3(0, 0, -0.2)
+        line.firstMaterial?.diffuse.contents = UIColor.yellow
+        line2.firstMaterial?.diffuse.contents = UIColor.yellow
+        line3.firstMaterial?.diffuse.contents = UIColor.yellow
+        line4.firstMaterial?.diffuse.contents = UIColor.yellow
+        line5.firstMaterial?.diffuse.contents = UIColor.yellow
+        line6.firstMaterial?.diffuse.contents = UIColor.yellow
         
-        boxNode1.geometry = box
-        boxNode1.position = SCNVector3(0, 0, -0.4)
         
-        boxNode2.geometry = box
-        boxNode2.position = SCNVector3(0, -0.1, -0.1)
+        let lineNode = SCNNode()
+        let lineNode2 = SCNNode()
+        let lineNode3 = SCNNode()
+        let lineNode4 = SCNNode()
+        let lineNode5 = SCNNode()
+        let lineNode6 = SCNNode()
+        let lineNode7 = SCNNode()
+        let lineNode8 = SCNNode()
+        let lineNode9 = SCNNode()
+        let lineNode10 = SCNNode()
+        let lineNode11 = SCNNode()
+        
+        lineNode.geometry = line
+        lineNode2.geometry = line2
+        lineNode3.geometry = line3
+        lineNode4.geometry = line4
+        lineNode5.geometry = line5
+        lineNode6.geometry = line6
+        lineNode7.geometry = line5
+        lineNode8.geometry = line4
+        lineNode9.geometry = line3
+        lineNode10.geometry = line2
+        lineNode11.geometry = line
+        
+        
+        lineNode.position = SCNVector3(0, 0, 0)
+        lineNode2.position = SCNVector3(0, -0.01, 0)
+        lineNode3.position = SCNVector3(0, -0.025, 0)
+        lineNode4.position = SCNVector3(0.015, -0.04, 0)
+        lineNode5.position = SCNVector3(0.03, -0.05, 0)
+        lineNode6.position = SCNVector3(0.045, -0.06, 0)
+        lineNode7.position = SCNVector3(0.03, -0.07, 0)
+        lineNode8.position = SCNVector3(0.015, -0.08, 0)
+        lineNode9.position = SCNVector3(0, -0.095, 0)
+        lineNode10.position = SCNVector3(0, -0.11, 0)
+        lineNode11.position = SCNVector3(0, -0.12, 0)
         
         
         let scene = SCNScene()
-        scene.rootNode.addChildNode(boxNode)
-        scene.rootNode.addChildNode(boxNode1)
-        scene.rootNode.addChildNode(boxNode2)
+        
+        scene.rootNode.addChildNode(lineNode)
+        scene.rootNode.addChildNode(lineNode2)
+        scene.rootNode.addChildNode(lineNode3)
+        scene.rootNode.addChildNode(lineNode4)
+        scene.rootNode.addChildNode(lineNode5)
+        scene.rootNode.addChildNode(lineNode6)
+        scene.rootNode.addChildNode(lineNode7)
+        scene.rootNode.addChildNode(lineNode8)
+        scene.rootNode.addChildNode(lineNode9)
+        scene.rootNode.addChildNode(lineNode10)
+        scene.rootNode.addChildNode(lineNode11)
         sceneView.scene = scene
     }
     
+//         ██████████
+//     ██████████████████
+//   ██████████████████████
+//   ██████████████████████
+//       ████████████████████
+//             ██████████████
+//                   ████████
+//             ██████████████
+//       ████████████████████
+//   ██████████████████████
+//   ██████████████████████
+//     ██████████████████
+//         ██████████
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
